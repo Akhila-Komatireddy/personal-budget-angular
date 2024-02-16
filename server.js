9493ddc7c8d2f2272cfbdd240f2462fcc1ac814c
Akhila-Komatireddy <146915965+Akhila-Komatireddy@users.noreply.github.com>
2024-02-15 21:08:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 const budget = require("./data.json");
 app.use("/",express.static("public"));
+
+app.use( cors() );
 
 console.log(budget)
 
